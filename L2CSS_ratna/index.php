36bd844d9231199b4a1867462793fff0b20ca190
Ratnadwas <rattnaas27@gmail.com>
2024-05-06 15:15:35 +0700

    L2CSS

---
+++ L2CSS_ratna/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS Card</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-img">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjADB8VsjHp8FaKQUJZvYubzBsBQXL6vlteCZSksDRZA&" alt="">
+            </div>
+
+            <div class="card-body">
+
+                <h2 class="card-tittle">NMIXX</h2>
+                <p class="card intro">
+                    NMIXX adalah Girl Grup besutan dari agensi BIG 3 JYP ENTERTAIMENT
+                    mempumyai satu satunya lagu debut dengan tema mixx popnya berjudul OO.
+                    Memiliki 6 anggota yang berbakat dan cantik. 
+                    
+                </p>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
